96142b05209385e412f71ab3427c533cdaa04fc9
Choi mieun <45choi22@gmail.com>
2026-03-08 13:44:46 +0900

    ♻️ Refactor: BaseException 상태 코드 추가 및 예외 처리 구조 개선
    BaseException에 상태 코드 필드를 추가하여 예외 응답 구조를 확장
    GlobalExceptionHandler에서 상태 코드를 활용하도록 수정하여
    예외 처리 흐름을 일관되게 관리하도록 개선
    
    Related to: #87

--- src/main/java/com/team/project/domain/payment/exception/InvalidPaymentRequestException.java
+++ src/main/java/com/team/project/domain/payment/exception/InvalidPaymentRequestException.java
@@ -1,10 +1,12 @@
 package com.team.project.domain.payment.exception;
 
+import org.springframework.http.HttpStatus;
+
 import com.team.project.global.common.exception.BaseException;
 
 public class InvalidPaymentRequestException extends BaseException {
 
 	public InvalidPaymentRequestException() {
-		super("INVALID_PAYMENT_REQUEST");
+		super("INVALID_PAYMENT_REQUEST", HttpStatus.BAD_REQUEST);
 	}
 }
