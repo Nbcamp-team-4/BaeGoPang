bad7d1c9bfbf239383b75b03c66d9e3fbbf42cc6
kjs0406 <rnjswlstjr19089@gmail.com>
2026-03-09 02:22:00 +0900

    feat : SecurityConfig bean 추가

--- src/main/java/com/team/project/global/config/SecurityConfig.java
+++ src/main/java/com/team/project/global/config/SecurityConfig.java
@@ -2,9 +2,13 @@ package com.team.project.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -19,5 +23,15 @@ public class SecurityConfig {
 				.anyRequest().permitAll()
 			);
 		return http.build();
+	}
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public AuthenticationManager authenticationManager(
+			AuthenticationConfiguration configuration) throws Exception {
+		return configuration.getAuthenticationManager();
 	}
 }
