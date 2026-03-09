d941c62ac3cf61f66eaae84804fe141d59c331bd
m1nj1m <lovekmj23@gmail.com>
2026-03-10 05:41:49 +0900

    🔧 fix: 배포 도메인 CORS 허용 설정 추가

--- src/main/java/com/team/project/global/config/WebConfig.java
+++ src/main/java/com/team/project/global/config/WebConfig.java
@@ -25,10 +25,14 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-			.allowedOrigins("http://localhost:3000")
-			.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
-			.allowedHeaders("*")
-			.allowCredentials(true);
+				.allowedOrigins(
+						"http://localhost:3000",
+						"https://baegopang.kro.kr",
+						"http://baegopang.kro.kr"
+				)
+				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
+				.allowedHeaders("*")
+				.allowCredentials(true);
 	}
 
 	@Override
