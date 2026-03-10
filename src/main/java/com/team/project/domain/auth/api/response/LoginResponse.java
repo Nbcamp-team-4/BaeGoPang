744601017c1fa2aebe10872a96fb8a034080563e
Choi mieun <45choi22@gmail.com>
2026-03-11 04:01:35 +0900

    Feat: 로그인 응답 필드 값 추가 로그인, 회원가입 시, 사용자 ID도 같이 응답하게 했습니다.

--- src/main/java/com/team/project/domain/auth/api/response/LoginResponse.java
+++ src/main/java/com/team/project/domain/auth/api/response/LoginResponse.java
@@ -1,6 +1,7 @@
 package com.team.project.domain.auth.api.response;
 
 import java.util.List;
+import java.util.UUID;
 
 import com.team.project.domain.user.entity.RoleType;
 
@@ -18,16 +19,19 @@ public class LoginResponse {
 	@Schema(description = "유저 정보")
 	private UserInfo user;
 
-	public LoginResponse(String accessToken, String refreshToken, String loginId, String name, List<RoleType> roles) {
+	public LoginResponse(String accessToken, String refreshToken, UUID id, String loginId, String name,
+		List<RoleType> roles) {
 		this.accessToken = accessToken;
 		this.refreshToken = refreshToken;
-		this.user = new UserInfo(loginId, name, roles);
+		this.user = new UserInfo(id, loginId, name, roles);
 	}
 
 	@Getter
 	@AllArgsConstructor
 	public static class UserInfo {
 
+		@Schema(description = "사용자 ID", type = "uuid")
+		private UUID id;
 		@Schema(description = "로그인 ID", example = "testid12")
 		private String loginId;
 		@Schema(description = "이름", example = "홍길동")
