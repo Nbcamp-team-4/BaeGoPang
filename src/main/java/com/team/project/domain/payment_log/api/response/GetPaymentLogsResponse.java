186bbb070620882a055f22552c1fcdb3c0ee686a
Choi mieun <45choi22@gmail.com>
2026-03-11 02:23:26 +0900

    Feat: 결제 로그 전체 조회 응답값 변경
    기존: ID만 반환
    변경 후: 로그 상태, 로그 실패 이유, 관련 결제 정보 추가

--- src/main/java/com/team/project/domain/payment_log/api/response/GetPaymentLogsResponse.java
+++ src/main/java/com/team/project/domain/payment_log/api/response/GetPaymentLogsResponse.java
@@ -1,13 +1,16 @@
 package com.team.project.domain.payment_log.api.response;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
+import com.team.project.domain.payment.model.vo.PaymentStatus;
 import com.team.project.domain.payment_log.model.dto.GetPaymentLogsQuery;
+import com.team.project.domain.payment_log.model.vo.PaymentLogStatus;
 import com.team.project.global.common.dto.BasePageResponse;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.Builder;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Schema(description = "결제 목록 조회 응답")
@@ -22,9 +25,8 @@ public class GetPaymentLogsResponse extends BasePageResponse<GetPaymentLogsRespo
 
 		List<Item> items = query.getContent()
 			.stream()
-			.map(item -> Item.builder()
-				.id(item.getId()).
-				build())
+			.map(item -> new Item(item.getPamentLogid(), item.getPaymentLogStatus(), item.getPaymentLogReason(),
+				item.getPaymentId(), item.getPaymentStatus(), item.getPaymentAmount(), item.getPaidAt()))
 			.toList();
 
 		return new GetPaymentLogsResponse(
@@ -36,11 +38,23 @@ public class GetPaymentLogsResponse extends BasePageResponse<GetPaymentLogsRespo
 	}
 
 	@Getter
-	@Builder
+	@AllArgsConstructor
 	@Schema(description = "결제 로그 목록 항목")
 	static public class Item {
 		@Schema(description = "결제 로그 ID", format = "uuid")
-		private UUID id;
+		private UUID pamentLogid;
+		@Schema(description = "결제 로그 상태")
+		private PaymentLogStatus paymentLogStatus;
+		@Schema(description = "결제 로그 실패 이유")
+		private String paymentLogReason;
+		@Schema(description = "결제 ID", format = "uuid")
+		private UUID paymentId;
+		@Schema(description = "결제 상태")
+		private PaymentStatus paymentStatus;
+		@Schema(description = "결제 금액")
+		private Integer paymentAmount;
+		@Schema(description = "결제 일시")
+		private LocalDateTime paidAt;
 	}
 }
 
