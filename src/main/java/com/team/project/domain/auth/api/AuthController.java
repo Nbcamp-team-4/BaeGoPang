ca6ff30a85a15038e0f69d891ec26f0169c40f91
kjs0406 <rnjswlstjr19089@gmail.com>
2026-03-10 17:01:48 +0900

    feat : 회원가입 swagger 추가

--- src/main/java/com/team/project/domain/auth/api/AuthController.java
+++ src/main/java/com/team/project/domain/auth/api/AuthController.java
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,6 +24,7 @@ import com.team.project.domain.auth.service.AuthService;
 
 import lombok.RequiredArgsConstructor;
 
+@Tag(name = "auth", description = "auth API")
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
@@ -30,16 +32,74 @@ public class AuthController {
 
 	private final AuthService authService;
 
+	@Operation(summary = "로그인", description = "로그인 합니다.")
+	@ApiResponses(value = {
+			@io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "로그인 성공"),
+			@io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "400", description = "잘못된 요청", content = @Content(
+					mediaType = "application/json",
+					schema = @Schema(implementation = ResponseEntity.class),
+					examples = @io.swagger.v3.oas.annotations.media.ExampleObject(
+							value = """
+					  {
+					        "Bad Request"
+					  }
+					"""
+					))),
+			@io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "404", description = "해당 정보 찾을 수 없음", content = @Content(
+					mediaType = "application/json",
+					schema = @Schema(implementation = ResponseEntity.class),
+					examples = @io.swagger.v3.oas.annotations.media.ExampleObject(
+							value = """
+					  {
+					        "Not Found"
+					  }
+					"""
+					)
+			))})
 	@PostMapping("/login")
 	public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest request) {
 		return ResponseEntity.ok(authService.login(request));
 	}
-
+	@Operation(summary = "리프래시 토큰 생성", description = "리프래시 토큰 생성합니다.")
+	@ApiResponses(value = {
+			@io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "201", description = "토큰 생성 성공"),
+			@io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "400", description = "잘못된 요청", content = @Content(
+					mediaType = "application/json",
+					schema = @Schema(implementation = ResponseEntity.class),
+					examples = @io.swagger.v3.oas.annotations.media.ExampleObject(
+							value = """
+					  {
+					        "Bad Request"
+					  }
+					"""
+					)))})
 	@PostMapping("/reissue")
 	public ResponseEntity<LoginResponse> reissue(@RequestHeader("Refresh-Token") String refreshToken) {
 		return ResponseEntity.ok(authService.reissue(refreshToken));
 	}
-
+	@Operation(summary = "로그 아웃", description = "로그아웃합니다.")
+	@ApiResponses(value = {
+			@io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "로그아웃 성공"),
+			@io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "400", description = "잘못된 요청", content = @Content(
+					mediaType = "application/json",
+					schema = @Schema(implementation = ResponseEntity.class),
+					examples = @io.swagger.v3.oas.annotations.media.ExampleObject(
+							value = """
+					  {
+					        "Bad Request"
+					  }
+					"""))),
+			@io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "404", description = "해당 정보 찾을 수 없음", content = @Content(
+			mediaType = "application/json",
+			schema = @Schema(implementation = ResponseEntity.class),
+			examples = @io.swagger.v3.oas.annotations.media.ExampleObject(
+					value = """
+					  {
+					        "Not Found"
+					  }
+					"""
+			)
+	))})
 	@PostMapping("/logout")
 	public ResponseEntity<Void> logout(@CurrentUser UserDto userDto) {
 		authService.logout(userDto.getId());
